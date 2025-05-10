c4f165357098d31ac2149a69e49193a9f41a73a5
DmytroMitin <dmitin3@gmail.com>
2025-05-11 01:18:00 +0300

    Add intermediate trait

--- src/main/scala/DiscriminationCriteria.scala
+++ src/main/scala/DiscriminationCriteria.scala
@@ -4,10 +4,14 @@ trait DiscriminationCriteria[-S] {
   transparent inline def discriminator[P <: S]: Int
 }
 
+trait DiscriminationCriteria1[-S] extends DiscriminationCriteria[S] {
+  transparent inline def discriminator[P <: S]: Int = ???
+}
+
 transparent inline def someMacro[SumType, VariantType]: Unit = ${ someMacroImpl[SumType, VariantType] }
 
 def someMacroImpl[SumType: Type, VariantType: Type](using quotes: Quotes): Expr[Unit] = {
-  val discriminatorExpr: Expr[Int] = Expr.summon[DiscriminationCriteria[SumType]] match {
+  val discriminatorExpr: Expr[Int] = Expr.summon[DiscriminationCriteria1[SumType]] match {
     case Some(discriminatorCriteriaExpr) =>
       '{ $discriminatorCriteriaExpr.discriminator[VariantType & SumType] }
     case None =>
