1e16d412d9cfa5240f9b5c5aaeb774cc0f1c204a
DmytroMitin <dmitin3@gmail.com>
2025-05-11 01:21:01 +0300

    Add inline helper

--- src/main/scala/DiscriminationCriteria.scala
+++ src/main/scala/DiscriminationCriteria.scala
@@ -1,19 +1,19 @@
+import scala.compiletime.summonInline
 import scala.quoted.{Expr, Quotes, Type}
 
 trait DiscriminationCriteria[-S] {
   transparent inline def discriminator[P <: S]: Int
 }
 
-trait DiscriminationCriteria1[-S] extends DiscriminationCriteria[S] {
-  transparent inline def discriminator[P <: S]: Int = ???
-}
+inline def summonDiscriminator[SumType, VariantType <: SumType]: Int =
+  summonInline[DiscriminationCriteria[SumType]].discriminator[VariantType]
 
 transparent inline def someMacro[SumType, VariantType]: Unit = ${ someMacroImpl[SumType, VariantType] }
 
 def someMacroImpl[SumType: Type, VariantType: Type](using quotes: Quotes): Expr[Unit] = {
-  val discriminatorExpr: Expr[Int] = Expr.summon[DiscriminationCriteria1[SumType]] match {
-    case Some(discriminatorCriteriaExpr) =>
-      '{ $discriminatorCriteriaExpr.discriminator[VariantType & SumType] }
+  val discriminatorExpr: Expr[Int] = Expr.summon[DiscriminationCriteria[SumType]] match {
+    case Some(_) =>
+      '{ summonDiscriminator[SumType, VariantType & SumType] }
     case None =>
       // Fall back to the alphanumerical index
       Expr[Int](??? /*alphanumericIndex*/)
